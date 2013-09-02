d88eb4b97f451d2ada55a0b6bcea7e5050899b1e
Marcel Šulek <marcel@sulek.eu>
2013-09-02 16:20:02 +0200

    zakladni nakresleni cele mapy

--- www/js/World.js
+++ www/js/World.js
@@ -1,12 +1,50 @@
 (function(){
-  var Worldmap;
+  var Dimensionable, Worldmap;
   window.init = function(data){
-    return console.log(data);
+    console.log(data);
+    return new Worldmap;
+  };
+  Dimensionable = {
+    margin: {
+      top: 10,
+      right: 0,
+      bottom: 22,
+      left: 39
+    },
+    computeDimensions: function(fullWidth, fullHeight){
+      this.fullWidth = fullWidth;
+      this.fullHeight = fullHeight;
+      this.width = this.fullWidth - this.margin.left - this.margin.right;
+      return this.height = this.fullHeight - this.margin.top - this.margin.bottom;
+    }
   };
   Worldmap = (function(){
     Worldmap.displayName = 'Worldmap';
     var prototype = Worldmap.prototype, constructor = Worldmap;
-    function Worldmap(data){}
+    importAll$(prototype, arguments[0]);
+    function Worldmap(data){
+      var x$, y$, z$, this$ = this;
+      this.computeDimensions(600, 500);
+      x$ = this.projection = d3.geo.mercator();
+      x$.scale(this.width / Math.PI / 2);
+      x$.translate([this.width / 2, this.height / 2]);
+      x$.precision(0.1);
+      y$ = this.path = d3.geo.path();
+      y$.projection(this.projection);
+      z$ = this.svg = d3.select('body').append('svg');
+      z$.attr('width', this.fullWidth);
+      z$.attr('height', this.fullHeight);
+      d3.json("./js/world-50m.json", function(err, world){
+        this$.svg.append('path').datum(topojson.feature(world, world.objects.land)).attr('class', 'land').attr('d', this$.path);
+        return this$.svg.append('path').datum(topojson.mesh(world, world.objects.countries, function(a, b){
+          return a !== b;
+        })).attr('class', 'boundary').attr('d', this$.path);
+      });
+    }
     return Worldmap;
-  }());
+  }(Dimensionable));
+  function importAll$(obj, src){
+    for (var key in src) obj[key] = src[key];
+    return obj;
+  }
 }).call(this);
--- www/ls/World.ls
+++ www/ls/World.ls
@@ -1,5 +1,38 @@
 window.init = (data) ->
     console.log data
+    new Worldmap
 
-class Worldmap
+Dimensionable =
+    margin:
+        top: 10
+        right: 0
+        bottom: 22
+        left: 39
+    computeDimensions: (@fullWidth, @fullHeight) ->
+        @width = @fullWidth - @margin.left - @margin.right
+        @height = @fullHeight - @margin.top - @margin.bottom
+
+
+
+class Worldmap implements Dimensionable
     (data) ->
+        @computeDimensions 600 500
+        @projection = d3.geo.mercator!
+            ..scale @width / Math.PI / 2
+            ..translate [@width / 2, @height / 2]
+            ..precision 0.1
+        @path = d3.geo.path!
+            ..projection @projection
+        @svg = d3.select \body .append \svg
+            ..attr \width @fullWidth
+            ..attr \height @fullHeight
+        (err, world) <~ d3.json "./js/world-50m.json"
+        @svg.append \path
+            .datum topojson.feature world, world.objects.land
+            .attr \class \land
+            .attr \d @path
+        @svg.append \path
+            .datum topojson.mesh world, world.objects.countries, (a, b) -> a isnt b
+            .attr \class \boundary
+            .attr \d @path
+
